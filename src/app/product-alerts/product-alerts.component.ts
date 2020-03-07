05526559c85f174998731dda3f7d0739620e1642
Felipe Romero <feliperomero3@outlook.com>
2020-03-07 14:52:58 -0600

    Display the product alert component as a child of the product list.
    Product alerts component emits an event when user clicks "Notify Me".

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  templateUrl: './product-alerts.component.html'
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
