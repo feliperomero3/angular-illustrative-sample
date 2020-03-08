59c68796ed31587a593aab8b2721b24b4ba9aced
Felipe Romero <feliperomero3@outlook.com>
2020-03-07 21:07:41 -0600

    Add method to add items to the cart.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,4 +5,8 @@ import { Injectable } from "@angular/core";
 })
 export class CartService {
   items = [];
+
+  addToCart(product: { name: string; price: number; description: string; }): void {
+    this.items.push(product);
+  }
 }
