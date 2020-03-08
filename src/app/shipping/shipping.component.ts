cab4bf06a5ae35f5c714ada6be2eb01a175b60ee
Felipe Romero <feliperomero3@outlook.com>
2020-03-07 23:03:23 -0600

    Modify the shipping component to use the cart service to retrieve shipping data.

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from "@angular/core";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-shipping",
   templateUrl: "./shipping.component.html"
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts: any;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
