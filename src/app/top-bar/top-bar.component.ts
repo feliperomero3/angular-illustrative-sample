c80706cc3379d0774ddfebe98e3ba943f5a962bb
Felipe Romero <feliperomero3@outlook.com>
2020-03-10 00:04:08 -0600

    Remove component's empty styles directive.

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -2,9 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-top-bar",
-  templateUrl: "./top-bar.component.html",
-  styles: []
+  templateUrl: "./top-bar.component.html"
 })
-export class TopBarComponent {
-
-}
+export class TopBarComponent { }
